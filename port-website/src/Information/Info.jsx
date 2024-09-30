3f54d153c66be6389f079db8ce435f66eb2b2627
Sakshi Jain <Sakshi@9897.in>
2024-09-30 14:58:08 +0530

    add resume

--- port-website/src/Information/Info.jsx
+++ port-website/src/Information/Info.jsx
@@ -1,27 +1,251 @@
 import React from 'react'
-
-
+import './inf.css'
 function Info() {
   return (
     <>
-            <h1 className='text-center justify-center mb-20 mt-28 text-4xl h1pro'>About me</h1>
-<div className='justify-center items-center flex'>
-<a href="#" className="flex flex-col items-center justify-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-[#06060f] dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/src/image-4.jpg" alt=""/>
-    <div className="flex flex-col justify-between p-4 leading-normal">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    </div>
-</a>
+            <h1 className='text-center justify-center  mt-28 text-4xl h1pro'>About me</h1>
+             <section id="resume" className="resume">
+            <div className="container">
+                <div className="row">
+<div className="col-12">
+<div className="resume-wrapper">
+<div className="section-heading-middle">
+<div className="sub-heading d-flex align-items-center mx-auto">
+
 </div>
 
+</div>
+<div className="row mt-50 row-mobile-margin">
+<div className="col-12 col-lg-2">
+<div
+    className="resume-topics h-100 flex-column justify-content-center position-relative">
+    <h3
+        className="h3 line-height-3 resume-topic-item resume-topic-one fw-500 text-white position-absolute">
+        Education</h3>
+    <h3
+        className="h3 line-height-3 resume-topic-item resume-topic-two fw-500 text-white position-absolute">
+        Software Skills
+    </h3>
 
-    <div className='flex m-20 justify-evenly  '>
+</div>
+</div>
+<div className="col-12 col-lg-10">
+{/* <!-- resume education row --> */}
+<div data-aos="fade-up" data-aos-delay="100" data-aos-duration="800"
+    className="resume-education bg-[#11111b]">
+    <div className="row">
+        {/* <!-- education first column --> */}
+        <div className="col-lg-4 resume-first-item">
+            <h4 className="h4 text-white fw-500">
+                B-Tech in Computer Science & Engineering
+            </h4>
+            <p className="p secondary-black fw-400 line-height-7 mt-10">
+                Maharshi Dayananad University (2021 - 2024)
+            </p>
+            <p className="p secondary-black fw-400 line-height-7 mt-4 text-lg">
+              learnings:  <b>software development,</b> <b>web technologies</b>, and <b>data structures</b>, providing a strong foundation for my career as a full-stack web developer
+            </p>
+        </div>
+        {/* <!-- education second column --> */}
+        <div className="col-lg-4 resume-second-item">
+            <h4 className="h4 text-white fw-500">
+                Diploma in Digital Electronics
+            </h4>
+            <p className="p secondary-black fw-400 line-height-7 mt-10">
+                Borad Of Technical Education (2017 - 2020)
+            </p>
+            <p className="p secondary-black fw-400 line-height-7 mt-4 text-lg">
+            Gained a solid understanding of programming, software development, and web technologies, laying the groundwork for my journey as a full-stack web developer.
+            </p>
+        </div>
+        {/* <!-- education third column --> */}
+        <div className="col-lg-4 resume-third-item">
+            <h4 className="h4 text-white fw-500">
+                Secondary School Education
+            </h4>
+            <p className="p secondary-black fw-400 line-height-7 mt-10">
+                VPS(2016 - 2017)
+            </p>
+            <p className="p secondary-black fw-400 line-height-7 mt-4 text-lg">
+            Completed with a focus on foundational subjects in mathematics, science, and computer studies, sparking my interest in technology and software development
+            </p>
+        </div>
+    </div>
+</div>
+{/* <!-- resume skills row --> */}
+<div data-aos="fade-up" data-aos-delay="200" data-aos-duration="800"
+    className="resume-skills bg-[#11111b] mt-4">
+    <div className="row">
+        {/* <!-- skills first column --> */}
+        <div className="col-lg-4 resume-first-item">
+            <h4 className="h4 text-white fw-500">
+                CourseWork
+            </h4>
+            <div className="row mt-10 justify-content-between">
+                <p
+                    className="col-10 p secondary-black fw-400 line-height-7 skill-name position-relative">
+                    C++
+                </p>
+                <p className="col-2 p secondary-black fw-400 line-height-7">
+                    80%
+                </p>
+            </div>
+            <div className="row mt-10 justify-content-between">
+                <p
+                    className="col-10 p secondary-black fw-400 line-height-7 skill-name position-relative">
+                    Data Structures & Algoritms
+                </p>
+                <p className="col-2 p secondary-black fw-400 line-height-7">
+                    70%
+                </p>
+            </div>
+            <div className="row mt-10 justify-content-between">
+                <p
+                    className="col-10 p secondary-black fw-400 line-height-7 skill-name position-relative">
+                    Computer Networking 
+                </p>
+                <p className="col-2 p secondary-black fw-400 line-height-7">
+                    70%
+                </p>
+            </div>
+            <div className="row mt-10 justify-content-between">
+                <p
+                    className="col-10 p secondary-black fw-400 line-height-7 skill-name position-relative">
+                    Operating system
+                </p>
+                <p className="col-2 p secondary-black fw-400 line-height-7">
+                    70%
+                </p>
+            </div>
+            
+            <div className="row justify-content-between mt-10">
+                <p
+                    className="p secondary-black fw-400 line-height-7 col-10 skill-name position-relative">
+                    Oops Concept
+                </p>
+                <p className="p col-2 secondary-black fw-400 line-height-7">
+                    80%
+                </p>
+            </div>
+        </div>
+        {/* <!-- skills second column --> */}
+        <div className="col-lg-4 resume-second-item">
+            <h4 className="h4 text-white fw-500">
+                Web Developement
+            </h4>
+            
+            <div className="row justify-content-between mt-10">
+                <p
+                    className="p secondary-black fw-400 line-height-7 col-10 skill-name position-relative">
+                    React.js
+                </p>
+                <p className="p col-2 secondary-black fw-400 line-height-7">
+                    80%
+                </p>
+            </div>
+            <div className="row justify-content-between mt-10">
+                <p
+                    className="p secondary-black fw-400 line-height-7 col-10 skill-name position-relative">
+                    Node.js
+                </p>
+                <p className="p col-2 secondary-black fw-400 line-height-7">
+                    80%
+                </p>
+            </div>
+            <div className="row justify-content-between mt-10">
+                <p
+                    className="p secondary-black fw-400 line-height-7 col-10 skill-name position-relative">
+                    Express.js
+                </p>
+                <p className="p col-2 secondary-black fw-400 line-height-7">
+                    80%
+                </p>
+            </div>
+            <div className="row justify-content-between mt-10">
+                <p
+                    className="p secondary-black fw-400 line-height-7 col-10 skill-name position-relative">
+                    Mysql
+                </p>
+                <p className="p col-2 secondary-black fw-400 line-height-7">
+                    90%
+                </p>
+            </div>
+            <div className="row justify-content-between mt-10">
+                <p
+                    className="p secondary-black fw-400 line-height-7 col-10 skill-name position-relative">
+                    MongoDB
+                </p>
+                <p className="p col-2 secondary-black fw-400 line-height-7">
+                    90%
+                </p>
+            </div>
+        </div>
+        {/* <!-- skills thirdt column --> */}
+        <div className="col-lg-4 resume-third-item">
+            <h4 className="h4 text-white fw-500">
+                Web Designing
+            </h4>
+            <div className="row mt-10 justify-content-between">
+                <p
+                    className="col-10 p secondary-black fw-400 line-height-7 skill-name position-relative">
+                    HTML5
+                </p>
+                <p className="col-2 p secondary-black fw-400 line-height-7">
+                    92%
+                </p>
+            </div>
+            <div className="row justify-content-between mt-10">
+                <p
+                    className="p secondary-black fw-400 line-height-7 col-10 skill-name position-relative">
+                    CSS
+                </p>
+                <p className="p col-2 secondary-black fw-400 line-height-7">
+                    87%
+                </p>
+            </div>
+            <div className="row justify-content-between mt-10">
+                <p
+                    className="p secondary-black fw-400 line-height-7 col-10 skill-name position-relative">
+                    JavaScript
+                </p>
+                <p className="p col-2 secondary-black fw-400 line-height-7">
+                    87%
+                </p>
+            </div>
+            <div className="row justify-content-between mt-10">
+                <p
+                    className="p secondary-black fw-400 line-height-7 col-10 skill-name position-relative">
+                    Bootstrap5
+                </p>
+                <p className="p col-2 secondary-black fw-400 line-height-7">
+                    82%
+                </p>
+            </div>
+            <div className="row justify-content-between mt-10">
+                <p
+                    className="p secondary-black fw-400 line-height-7 col-10 skill-name position-relative">
+                    Tailwind css
+                </p>
+                <p className="p col-2 secondary-black fw-400 line-height-7">
+                    82%
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+     </div>
+     </div>
+          </div>
+         </div>
+        </div>
+  </div>
+        </section>
+    <div className='flex  justify-center'>
       <div className="card">
   <div className="card-details">
      <i className="fa-solid fa-laptop text-center text-3xl custom-icon"></i> 
     <p className="text-title">Frontend Development</p>
-    <p className="text-body">Just like this portfolio website, I can make customised web apps and websites according to user's convinience and demands in variety of tech stacks!</p>
+    <p className="text-white">Just like this portfolio website, I can make customised web apps and websites according to user's convinience and demands in variety of tech stacks!</p>
   </div>
  <a href="https://github.com/sakshixjain"> <button className="card-button">More info</button></a>
 </div>
@@ -29,7 +253,7 @@ function Info() {
   <div className="card-details">
   <i className="fa-solid fa-gear text-3xl text-center custom-icon"></i>
     <p className="text-title">Backend Development</p>
-    <p className="text-body">Making backend from scratch, connecting with database or fixing bugs, you name it and I can do it, just give me the command and I'll try my best to give you a satisfied service!!</p>
+    <p className="text-white">Making backend from scratch, connecting with database or fixing bugs, you name it and I can do it, just give me the command and I'll try my best to give you a satisfied service!!</p>
   </div>
   <a href="https://github.com/sakshixjain"> <button className="card-button">More info</button></a>
 </div>
@@ -37,7 +261,15 @@ function Info() {
   <div className="card-details">
   <i className="fa-solid fa-code text-3xl text-center custom-icon"></i>
     <p className="text-title">Problem Solving</p>
-    <p className="text-body">I have a good command over variety of concepts in Data Structures and Algorithms using which I can solve bugs or implement solutions in programming language of your choice!</p>
+    <p className="text-white">I have a good command over variety of concepts in Data Structures and Algorithms using which I can solve bugs or implement solutions in programming language of your choice!</p>
+  </div>
+ <a href="https://leetcode.com/u/sakshijain36/"> <button className="card-button">More info</button></a>
+</div>
+<div className="card">
+  <div className="card-details">
+  <i className="fa-solid fa-code text-3xl text-center custom-icon"></i>
+    <p className="text-title">UI/UX Designer</p>
+    <p className="text-white">Being well versed with variety of concepts in UI/UX principles and technologies, I can design as well as implement your website's interface to optimize user experience to the fullest!</p>
   </div>
  <a href="https://leetcode.com/u/sakshijain36/"> <button className="card-button">More info</button></a>
 </div>
