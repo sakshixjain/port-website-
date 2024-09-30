0a2c1ac5a9d588f55c19c8d25cbb41af01ad6c32
Sakshi Jain <Sakshi@9897.in>
2024-09-30 21:46:21 +0530

    .

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -57,6 +57,54 @@ function Project () {
               <a href="https://github.com/sakshixjain"> <button className='skill-btn'>More info</button></a>
           
           </div>
+          <div className="image-item h-[430px]" >
+            <figure><img src="ecommerce.png" alt="Cinque Terre" className="image-scroll" /></figure>
+            
+              <p className='caption'>E-commerce Website</p>
+              <p className='text-wrap p-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis deserunt nam ea velit repellendus sequi dolorem fugit accusa aperiam?</p>
+              <a href="https://github.com/sakshixjain"> <button className='skill-btn mt-2'>More info</button></a>
+          </div>
+<div className="image-item h-[450px]" >
+            <figure><img src="alarm-clock.png" alt="Cinque Terre" className="image-scroll" /></figure>
+            
+              <p className="caption">Alarm Clock</p>
+             
+              <p className='text-wrap p-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis deserunt nam ea velit repellendus sequi dolorem fugit accusa aperiam?</p>
+              <a href="https://github.com/sakshixjain"> <button className='skill-btn mt-2'>More info</button></a>
+          </div>
+          <div className="image-item h-[440px]" >
+           <figure> <img src="todo-list.png" alt="Cinque Terre" className="image-scroll" /></figure>
+              <p className="caption">Responsive Todo App</p>
+            
+              <p className='text-wrap p-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis deserunt nam ea velit repellendus sequi dolorem fugit accusa aperiam?</p>
+              <a href="https://github.com/sakshixjain"> <button className='skill-btn '>More info</button></a>
+          </div>
+          <div className="image-item h-[440px]" >
+            <figure><img src="login-form.png" alt="Cinque Terre" className="image-scroll" /></figure>
+            
+              <p className="caption" >Login Form</p>
+            
+              <p className='text-wrap p-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis deserunt nam ea velit repellendus sequi dolorem fugit accusa aperiam?</p>
+              <a href="https://github.com/sakshixjain"> <button className='skill-btn'>More info</button></a>
+          </div>
+          <div className="image-item h-[422px]" >
+           <figure> <img src="chat-app.png" alt="Cinque Terre" className="image-scroll " /></figure>
+            
+              <p  className="caption">Chat Application</p>
+             
+              <p className='text-wrap p-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis deserunt nam ea velit repellendus sequi dolorem fugit accusa aperiam?</p>
+              <a href="https://github.com/sakshixjain"> <button className='skill-btn'>More info</button></a>
+          
+          </div>
+          <div className="image-item h-[485px]" >
+           <figure> <img src="calculator.png" alt="Cinque Terre" className="image-scroll" /></figure>
+          
+              <p className="caption">Tip Calculator App</p>
+              
+                <p className='text-wrap p-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis deserunt nam ea velit repellendus sequi dolorem fugit accusa aperiam?</p>
+              <a href="https://github.com/sakshixjain"> <button className='skill-btn'>More info</button></a>
+          
+          </div>
           </div>
           </div>
     </>
