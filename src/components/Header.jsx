a77ff96d5c6a2ffcb8315195f04167e857f26576
Sakshi Jain <Sakshi@9897.in>
2024-10-02 19:58:39 +0530

    header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,11 @@ function Header() {
   return (
     <>
     <div className='headerbody'>
-      <div className='mt-8 z-20 '>
+      <div className=' z-20 top-0  fixed'>
         <nav>
             <a href="#">Home</a>
             <a href="#">About</a>
+            <a href="#">Skills</a>
             <a href="#">Projects</a>
             <a href="#">Contact</a>
         </nav>
@@ -23,25 +24,25 @@ function Header() {
                     <p className='text-xl -ml-56 mt-1' style={{textShadow: '1px 1px 8px wheat'}}>I'm Sakshi Jain, a Full Stack Developer with a degree from Maharshi <br /> Dayanand University,(MDU) Rohtak. With a passion for clean  code <br /> and problem-solving,  I thrive in crafting seamless user experiences <br /> and efficient backend solutions.</p>   
                     <div className='flex -ml-64'>
                     <div className='icon-div'> 
-                   <a href="https://github.com/sakshixjain"><i className="fa-brands fa-github"></i></a> 
+                   <a href="https://github.com/sakshixjain"><i className="fa-brands fa-github "></i></a> 
                     <span>Github</span>
                     </div>
                     <div className='icon-div'>
-                    <i className="fa-brands fa-linkedin"></i>
-                    <span>Linked-in</span>
+                    <i className="fa-brands fa-linkedin "></i>
+                    <span>Linkedin</span>
                     </div>
                     <div className='icon-div'>
-                    <a href="https://leetcode.com/u/sakshijain36/"><i className="fa-solid fa-link"></i></a>
+                    <a href="https://leetcode.com/u/sakshijain36/"><i className="fa-solid fa-link "></i></a>
                     <span>Leetcode</span>
                     </div>  
                     <div className='icon-div'>
-                    <a href="https://www.geeksforgeeks.org/user/sakshijain36/"><i className="fa-solid fa-link"></i></a>
+                    <a href="https://www.geeksforgeeks.org/user/sakshijain36/"><i className="fa-solid fa-link "></i></a>
                     <span>G-For-G</span>
                     </div> 
 
                     </div>
  </div>
-              <img className='w-64 h-64 mr-28 rounded-full' src="pexels-fathi-vee-1020478.jpg" alt="" />
+              <img className='w-80 h-80 mr-28 rounded-s-3xl ' src="pexels-fathi-vee-1020478.jpg" alt="" />
 </div>/
     </>
   )
