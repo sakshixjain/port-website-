f7ab7f1f2549ee893f64b5d88fd60fd1638942ae
Sakshi Jain <Sakshi@9897.in>
2024-10-02 22:03:25 +0530

    changes in  icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,14 +21,14 @@ function Header() {
                 <li><span>Sakshi Jain</span></li>
                 <li><span>Full Stack Developer</span></li>
             </ul >
-                    <p className='text-xl -ml-56 mt-1' style={{textShadow: '1px 1px 8px wheat'}}>I'm Sakshi Jain, a Full Stack Developer with a degree from Maharshi <br /> Dayanand University,(MDU) Rohtak. With a passion for clean  code <br /> and problem-solving,  I thrive in crafting seamless user experiences <br /> and efficient backend solutions.</p>   
+                    <p className='text-xl -ml-56 mt-2' style={{textShadow: '1px 1px 8px wheat'}}>I'm Sakshi Jain, a Full Stack Developer with a degree from Maharshi <br /> Dayanand University,(MDU) Rohtak. With a passion for clean  code <br /> and problem-solving,  I thrive in crafting seamless user experiences <br /> and efficient backend solutions.</p>   
                     <div className='flex -ml-64'>
                     <div className='icon-div'> 
                    <a href="https://github.com/sakshixjain"><i className="fa-brands fa-github "></i></a> 
                     <span>Github</span>
                     </div>
                     <div className='icon-div'>
-                    <i className="fa-brands fa-linkedin "></i>
+                   <a href="#"><i className="fa-brands fa-linkedin "></i></a>
                     <span>Linkedin</span>
                     </div>
                     <div className='icon-div'>
@@ -37,10 +37,11 @@ function Header() {
                     </div>  
                     <div className='icon-div'>
                     <a href="https://www.geeksforgeeks.org/user/sakshijain36/"><i className="fa-solid fa-link "></i></a>
-                    <span>G-For-G</span>
+                    <span>GForG</span>
                     </div> 
-
+                    
                     </div>
+                    <button><a href="">cv<i className="fa-solid fa-download"></i></a></button>
  </div>
               <img className='w-80 h-80 mr-28 rounded-s-3xl ' src="pexels-fathi-vee-1020478.jpg" alt="" />
 </div>/
